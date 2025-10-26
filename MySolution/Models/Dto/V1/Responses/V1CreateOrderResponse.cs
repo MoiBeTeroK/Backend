4ff736fa9efbe802e0aaa3e4017c3fa705bb1e08
MoiBeTeroK <ludarikkot@gmail.com>
2025-10-26 23:44:13 +0300

    Lab1 Task1-43

---
+++ MySolution/Models/Dto/V1/Responses/V1CreateOrderResponse.cs
@@ -0,0 +1,7 @@
+using Models.Dto.Common;
+namespace Models.Dto.V1.Responses;
+
+public class V1CreateOrderResponse
+{
+    public OrderUnit[] Orders { get; set; }
+}
---
+++ MySolution/Models/Dto/V1/Responses/V1QueryOrdersResponse.cs
@@ -0,0 +1,7 @@
+using Models.Dto.Common;
+namespace Models.Dto.V1.Responses;
+
+public class V1QueryOrdersResponse
+{
+    public OrderUnit[] Orders { get; set; }
+}
